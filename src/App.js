a751b3a5dd8bda30f991e22269decb59684f23c7
Maye Jesuorobo <nayawes@yahoo.com>
2023-12-09 15:16:34 +0100

    setup login route and page as well as most of the functionality

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/menu" element={<Menu />} />
       <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
       </Routes>
       
     </Router>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,1 +1,55 @@
-// Write all the code here
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleClick = () => {
+        if(!email || !password) {
+           return setErrorMessage("Please provide both an email and a password.")
+        } else alert('It works')
+
+        setErrorMessage("")
+    }
+    return (
+    <>
+        <section className="login-box">
+            <h3>login</h3>
+            <div className="box">
+                <input 
+                type="email" 
+                placeholder="Email address" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                type="password" 
+                placeholder="Password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <div>
+                    <div className="check">
+                        <input type="checkbox" name="" id="" /><span>Remember me</span>
+                    </div>
+                    <a href="#">
+                        Forgot password?
+                    </a>
+                </div>
+                <Link to="/">
+                    <button className="login-button" onClick={handleClick} type="submit">Sign in</button>
+                </Link>
+                {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+            </div>
+            <p>Not a member? 
+                <a href="#">Register</a>
+            </p>
+        </section>
+    </>
+    )
+}
+
+export default Login
