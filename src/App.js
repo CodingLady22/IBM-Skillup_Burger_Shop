7246618573b1bdbe0bb8353c67b6efba918a05b0
Maye Jesuorobo <nayawes@yahoo.com>
2023-12-10 11:32:00 +0100

    add route for the contact page

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
       <Route path="/menu" element={<Menu />} />
       <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/contact" element={<Contact />} />
       </Routes>
       
     </Router>
