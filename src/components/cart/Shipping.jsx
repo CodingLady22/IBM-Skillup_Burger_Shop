87d059a96669ea8ee4c73cbb79c9fda8dd0b8b29
Maye Jesuorobo <nayawes@yahoo.com>
2023-12-12 12:58:14 +0100

    update code for missing shipping inputs: countries and states select option, and contact input

--- src/components/cart/Shipping.jsx
+++ src/components/cart/Shipping.jsx
@@ -1,42 +1,99 @@
-import React from "react";
-import { Country, State } from "country-state-city";
+import { useState } from "react";
 import Popup from 'reactjs-popup';
+import countries from "../../countriesData"
 
 const Shipping = () => {
+
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+
+  // useState for phone number pattern verification
+  const [phoneNumber, setPhoneNumber] = useState('')
+
+  const handleChange = (e) => {
+    let inputValue = e.target.value
+    setPhoneNumber(inputValue)
+  }
+
   return (
     <section className="shipping">
       <main>
         <h1>Shipping Details</h1>
         <form>
+          {/* House number input*/}
           <div>
             <label>H.No.</label>
-            <input type="text" placeholder="Enter House No." />
+            <input 
+              type="text" 
+              placeholder="Enter House No." 
+            />
           </div>
+          {/* City input */}
           <div>
             <label>City</label>
-            <input type="text" placeholder="Enter City" />
+            <input 
+              type="text" 
+              placeholder="Enter City" 
+            />
           </div>
+          {/* Country select */}
           <div>
-              {/* Compelte the code for the COUNTRY DROPDOWN*/}
             <label>Country</label>
-
-            <select>
-              <option value="">Country</option>
-// Enter the code here for country dropdown           
-                  </option>
-                ))}
+            <select 
+              value={selectedCountry} 
+              onChange={(e) => setSelectedCountry(e.target.value)}
+            >
+              <option value="">Select Country</option>
+              {countries.map(country => (
+                <option key={country.id} value={country.countryName}>
+                  {country.countryName}
+              </option>
+              ))}        
             </select>
           </div>
+              {/*State select*/}
           <div>
-              {/* Add the code for the STATE DROPDOWN*/}
-           
+              <label>State</label>
+              <select
+                value={selectedState}
+                onChange={(e) => setSelectedState(e.target.value)}
+              >
+                <option value="">Select State</option>
+                {selectedCountry && 
+                countries.find(country => country.countryName === selectedCountry)
+                ?.states.map(state => (
+                  <option
+                    key={state} 
+                    value={state}
+                  >
+                    {state}
+                  </option>
+                ))
+                }
+              </select>
           </div>
+          {/* Pin code input */}
           <div>
             <label>Pin Code</label>
-            <input type="number" placeholder="Enter Pincode" />
+            <input 
+              type="number" 
+              placeholder="Enter Pincode" 
+            />
           </div>
-        // Enter thr code for contact           
-          
+                {/* Phone number input */}
+          <div>
+            <label>Contact
+              <span>{phoneNumber.match(/[0-9]{3}-[0-9]{3}-[0-9]{4}/) ? '✓' : '✗'}</span>
+            </label>
+            <input 
+              type="tel" 
+              placeholder="123-456-7890" 
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              value={phoneNumber}
+              onChange={handleChange}
+              />
+          </div>        
+          {/* Submit button */}
           <Popup trigger=
                 {<button type = "button">Confirm Order</button>}
                 position="right center">
