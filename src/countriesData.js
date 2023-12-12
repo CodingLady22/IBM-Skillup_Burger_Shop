9787fb5d851c1292107fb5677554678fad109cb8
Maye Jesuorobo <nayawes@yahoo.com>
2023-12-12 12:52:34 +0100

    Create file containing names of some countries and some of their respective states

---
+++ src/countriesData.js
@@ -0,0 +1,70 @@
+
+
+const countriesData = [
+    {
+        id: 1,
+        countryName: "Australia",
+        states: [
+            "New South Wales", "Queensland", "South Australia", "Tasmania", "Victoria", "Western Australia"
+        ]
+    },
+    {
+        id: 2,
+        countryName: "Brazil",
+        states: [
+            "Amazonas", "Bahia", "Minas Gerais", "Rio de Janeiro", "Rio Grande do Sul", "Sao Paulo"
+        ]
+    },
+    {
+        id: 3,
+        countryName: "Canada",
+        states: [
+            "Alberta", "British Columbia", "Manitoba", "Nova Scotia", "Ontario", "Quebec"
+        ]
+    },
+    {
+        id: 4,
+        countryName: "India",
+        states: [
+            "Karnataka", "Kerala", "Maharashtra", "Tamil Nadu", "Uttar Pradesh", "West Bengal"
+        ]
+    },
+    {
+        id: 5,
+        countryName: "Italy",
+        states: [
+            "Campania", "Lazio", "Lombardy", "Sicily", "Tuscany", "Veneto"
+        ]
+    },
+    {
+        id: 6,
+        countryName: "Nigeria",
+        states: [
+            "Delta", "Edo", "Enugu", "Kano", "Lagos", "Rivers"
+        ]
+    },
+    {
+        id: 7,
+        countryName: "United Kingdom",
+        states: [
+            "Channel Islands", "England", "Isle of Man", "Northern Ireland", "Scotland", "Wales"
+        ]
+    },
+    {
+        id: 8,
+        countryName: "United States",
+        states: [
+            "California", "Colorado", "Florida", "Illinois", "New York", "Texas"
+        ]
+    },
+    {
+        id: 9,
+        countryName: "Venezuela",
+        states: [
+            "Capital District", "Carabobo", "Lara", "Miranda", "Táchira", "Yaracuy"
+        ]
+    },
+]
+
+
+export default countriesData
